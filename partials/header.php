eb980a884742914ce6546a4d261491ebf06db441
ValeraFabrice <hobyraobera@gmail.com>
2019-02-27 16:56:57 +0100

    voitures PHP

---
+++ partials/header.php
@@ -0,0 +1,78 @@
+<?php
+require_once __DIR__ . '/../config/function.php';
+require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../config/database.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>TP PHP/SQL</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">TP PHP/SQL</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+
+                <?php
+          // Gestion du menu dynamique
+          $menuItems = [
+            ['label' => 'Accueil', 'link' => 'index.php'],
+            ['label' => 'Ajouter une voiture', 'link' => 'category_list.php']
+          ];
+        ?>
+
+                <ul class="navbar-nav mr-auto">
+                    <?php foreach ($menuItems as $item) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo $item['link']; ?>">
+                            <?php echo $item['label']; ?>
+                        </a>
+                    </li>
+                    <?php } ?>
+                </ul>
+
+                <ul class="navbar-nav ml-auto">
+                    <?php if (isset($_SESSION['user'])) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"></a>
+                    </li>
+                    <?php } else { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="sign-up.php">
+                            Login
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">
+                            Inscription
+                        </a>
+                    </li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
---
+++ public/index.php
@@ -0,0 +1,47 @@
+<?php 
+require_once __DIR__ . '/../partials/header.php';
+?>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Photo</th>
+                <th scope="col">Marque</th>
+                <th scope="col">Modèle</th>
+                <th scope="col">Prix</th>
+                <th scope="col">Année de sortie</th>
+                <th scope="col">Actions</th>                             
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>
+                <img src="/../instructions/img/renault-megane.jpeg" alt=""> </td>
+                <td>Renault</td>
+                <td>Mégane</td>
+                <td>10 000,00 €</td>
+                <td>1995</td>
+                <td>Mark</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+                <td></td>
+                <td>Alfa Roméo</td>
+                <td>Giulia</td>
+                <td>30 000,00 €</td>
+                <td>2019</td>
+                <td>Mark</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+                <td></td>
+                <td>Porsche</td>
+                <td>Cayenne</td>
+                <td>80 000,00 €</td>
+                <td>2019</td>
+                <td>Mark</td>
+            </tr>
+        </tbody>
+    </table>
