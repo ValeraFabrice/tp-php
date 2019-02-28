9980d43d0bf2a0b49059de1225ff12bd9ae3f5e1
ValeraFabrice <hobyraobera@gmail.com>
2019-02-28 17:16:29 +0100

    car TP PHP

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+
+Connexion à la base de données
+
+Ce fichier permettra de faire la connexion entre PHP et MySQL avec PDO.
+
+*/
+
+define('HOST', 'localhost');
+define('DB_NAME', 'tp-php');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+
+$db = new PDO(
+    'mysql:host=' . HOST .';dbname=' . DB_NAME . ';charset=utf8',
+    DB_USER,
+    DB_PASSWORD,
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Active les erreurs SQL
+        // On récupère tous les résultats en tableau associatif
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]
+);
--- public/index.php
+++ public/index.php
@@ -1,47 +1,39 @@
 <?php 
 require_once __DIR__ . '/../partials/header.php';
+
+$query = $db->query('SELECT * FROM car');
+$cars = $query->fetchAll(); // [ ['id' => 1, 'name' => 'A'], ['id' => 2, 'name' => 'B'] ]
+
 ?>
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Photo</th>
-                <th scope="col">Marque</th>
-                <th scope="col">Modèle</th>
-                <th scope="col">Prix</th>
-                <th scope="col">Année de sortie</th>
-                <th scope="col">Actions</th>                             
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>
-                <img src="/../instructions/img/renault-megane.jpeg" alt=""> </td>
-                <td>Renault</td>
-                <td>Mégane</td>
-                <td>10 000,00 €</td>
-                <td>1995</td>
-                <td>Mark</td>
-            </tr>
-            <tr>
-            <th scope="row">2</th>
-                <td></td>
-                <td>Alfa Roméo</td>
-                <td>Giulia</td>
-                <td>30 000,00 €</td>
-                <td>2019</td>
-                <td>Mark</td>
-            </tr>
-            <tr>
-            <th scope="row">3</th>
-                <td></td>
-                <td>Porsche</td>
-                <td>Cayenne</td>
-                <td>80 000,00 €</td>
-                <td>2019</td>
-                <td>Mark</td>
-            </tr>
-        </tbody>
-    </table>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Photo</th>
+            <th scope="col">Marque</th>
+            <th scope="col">Modèle</th>
+            <th scope="col">Prix</th>
+            <th scope="col">Année de sortie</th>
+            <th scope="col">Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <?php foreach($cars as $car) { ?>
+            <th scope="row">1</th>
+            <td><img class="carphoto" src="../img/<?php echo$car['photo']; ?>" alt=""> </td>
+            <td><?php echo$car['brand']; ?></td>
+            <td><?php echo$car['model']; ?></td>
+            <td><?php echo$car['price']; ?></td>
+            <td><?php echo$car['release_date']; ?></td>
+            <td>
+                <a><i class=" btn btn-primary fas fa-pen"></i></a>
+                <a><i class=" btn btn-danger far fa-trash-alt"></i></a>
+            </td>
+        </tr>
+
+        <?php } ?>
+
+    </tbody>
+</table>
